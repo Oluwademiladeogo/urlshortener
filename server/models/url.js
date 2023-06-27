297628b5069e46116a7b427d63b347d04d580703
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2023-06-27 20:02:40 +0100

    added url route, working on redirect, logging now better and some other minor changes

---
+++ server/models/url.js
@@ -0,0 +1,9 @@
+module.exports.urlschema= mongoose.model( 'url', new mongoose.Schema({
+    longURL: String,
+    shortURL: String,
+    shortcode: String,
+    date: {
+        type: String,
+        default: Date.now
+    }
+}))
