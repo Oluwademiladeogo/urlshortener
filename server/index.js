7f7fe8aab6512ade4a91d5b97e19fbdb2e898238
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2023-06-27 15:16:56 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const winston = require('winston')
+const express = require('express')
+const app = express()
+const helmet = require('helmet')
+const joi = require("joi")
+const mongoose = require('mongoose')
+// const nanoid = require('nanoid')
+const nanoid = import('nanoid')
+app.use(express.static("./public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet())
+const schema = joi.object({
+    // url: joi.uri().requried()
+})
+const urlschema = mongoose.model( 'url', new mongoose.Schema({
+    longURL: String,
+    shortURL: String,
+    date: {
+        type: String,
+        default: Date.now
+    }
+}))
+app.post('/url', async(req, res)=>{
+    // const { error } = schema.validate(req.body.urlgotten)
+    // if(error) return res.send()
+    var shortURL = await nanoid
+    var longURL = req.body.urlgotten
+    let uri = new urlschema({
+        longURL: longURL,
+        shortURL: shortURL
+    })
+    const result = uri.save()
+
+    res.send(result)
+
+})
+const port = process.env.PORT || 3000
+winston.add(new winston.transports.File({filename: 'logfile.log' }))
+const dburi = "mongodb://localhost/27017"
+mongoose.connect(dburi, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(console.log('Connected to mongodb'))
+    .then(winston.info('Connected to mongodb'))
+
+app.listen(port, ()=>{
+    winston.info(`server running on port ${port}`)
+    console.log(`server running on port ${port}`)
+})
+
